c4e61cc245b8bcdb28bcdec3e2358d5d729f2112
Aisha25 <aishaalnozili83@gmail.com>
2024-02-13 12:11:46 +0300

    gg

--- WaterTracker/ContentView.swift
+++ WaterTracker/ContentView.swift
@@ -4,21 +4,75 @@
 //
 //  Created by Aisha Alnozili on 03/08/1445 AH.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userInput: String = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(alignment: .leading, spacing: 8) {
+                Image("drop")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80, height: 80)
+                    .padding(.bottom, 5)
+                
+                Text("iHydrate")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.leading)
+                
+                Text("Start with iHydrate to record and track your water intake daily based on your needs and stay hydrated")
+                    .foregroundColor(.gray)
+                
+                VStack(alignment: .leading, spacing: 2) {
+                    ZStack(alignment: .leading) {
+                        HStack {
+                            Text("Body weight")
+                                .foregroundColor(.black)
+                            Text("weight")
+                                .foregroundColor(.gray)
+                                .opacity(userInput.isEmpty ? 1.0 : 0.5)
+                            Text("Kg")
+                                .foregroundColor(.black)
+                        }
+                        TextField("", text: $userInput, onEditingChanged: { _ in }) {
+                            
+                        }
+                        .foregroundColor(.gray)
+                        .padding(.leading, 8)
+                        .opacity(userInput.isEmpty ? 0.5 : 1.0)
+                    }
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(8)
+                    .padding(.bottom, 8)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        print("tap")
+                    }) {
+                        Text("Calculate Now  ")
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(userInput.isEmpty ? Color.gray.opacity(0.5) : Color.lightBlue)
+                            .cornerRadius(10)
+                    }
+                    .disabled(userInput.isEmpty)
+                    .padding(.leading)
+                    .padding(.trailing)
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }

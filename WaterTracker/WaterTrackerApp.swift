39e35961fc45f7e8c2730cd6eca98186159b91bd
Aisha25 <aishaalnozili83@gmail.com>
2024-02-13 10:38:47 +0300

    Initial Commit

---
+++ WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by Aisha Alnozili on 03/08/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

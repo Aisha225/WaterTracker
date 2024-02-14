13671dc92bd86f2b4d14bcf583125767a5d931d9
Aisha25 <aishaalnozili83@gmail.com>
2024-02-14 22:13:06 +0300

    شوشو

---
+++ WaterTracker/ProgressTrack.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressTrack.swift
+//  WaterTracker
+//
+//  Created by Aisha Alnozili on 04/08/1445 AH.
+//
+
+import SwiftUI
+
+struct ProgressTrack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressTrack()
+}

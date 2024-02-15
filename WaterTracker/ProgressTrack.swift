26245381249e739eb06c881b217fc8dbdeb6dd74
Aisha25 <aishaalnozili83@gmail.com>
2024-02-15 11:43:04 +0300

    jju

--- WaterTracker/ProgressTrack.swift
+++ WaterTracker/ProgressTrack.swift
@@ -8,20 +8,33 @@
 import SwiftUI
 
 struct ProgressTrack: View {
-    @State private var progress: CGFloat = 0.7
+    @State private var progress: CGFloat = 0.0
     @State private var emojis = ["😴", "😃", "😁", "🥰", "🥳"]
     @State private var currentEmojiIndex: Int = 0
+    @State private var pressCount: Int = 0
     
-    func calculateWaterIntake() -> String {
-        
-        return ""
+    func updateProgress(increment: Bool) {
+        let incrementAmount: CGFloat = 0.1
+        if increment {
+            progress += incrementAmount
+        } else {
+            if pressCount < 5 {
+                progress -= incrementAmount
+                pressCount += 1
+            }
+        }
+        if progress > 1.0 {
+            progress = 1.0
+        } else if progress < 0.0 {
+            progress = 0.0
+        }
+        currentEmojiIndex = min(Int(progress * CGFloat(emojis.count)), emojis.count - 1)
     }
     
     var body: some View {
         VStack {
             Text(" Today's Water Intake")
-           
-                .padding(.trailing,180)
+                .padding(.trailing, 180)
                 .foregroundColor(Color("grey2"))
                 .offset(y: 60)
                 .padding(.leading)
@@ -40,87 +53,59 @@ struct ProgressTrack: View {
                     .foregroundColor(Color.lightBlue)
                     .rotationEffect(Angle(degrees: -90))
                 
-                 Text(emojis[currentEmojiIndex])
+                Text(emojis[currentEmojiIndex])
                     .font(.system(size: 40))
-                    .foregroundColor(Color.blue)
+                    .foregroundColor(Color.lightBlue2)
                     .offset(y: -160)
-                    .rotationEffect(Angle(degrees: Double(progress * 360 - 3)))
-                
+                    .rotationEffect(Angle(degrees: Double(progress * 360)))
+                    
             }
             .padding(40)
             
             Spacer()
             
-            HStack(spacing: 40) {
+            HStack(spacing: 130) {
                 Button(action: {
                     withAnimation {
-                        progress -= 0.1
-                        if progress < 0.0 {
-                            progress = 0.0
-                            
-                        }
-            else{
-                    currentEmojiIndex = Int(progress * CGFloat(emojis.count))
-                        }
-                        
+                        updateProgress(increment: false)
                     }
                 }) {
-                    
-                    HStack(spacing:180){
+                    ZStack {
+                        Circle()
+                            .fill(Color.grey3)
+                            .frame(width: 50, height: 50)
+                            .overlay(Circle().stroke(Color.grey3, lineWidth: 0))
                         
-                        ZStack {
-                            Circle()
-                                .fill(Color.grey3)
-                                .frame(width: 50, height: 50)
-                                .overlay(Circle().stroke(Color.grey3, lineWidth: 0))
-                            
-                            Image(systemName: "minus")
-                                .font(.system(size: 35))
-                                .foregroundColor(.lightBlue)
-                        }
-                    
-                    
-                    Button(action: {
-                        withAnimation {
-                            progress += 0.1
-                            if progress > 1.0 {
-                                progress = 1.0
-                            }
-                            else{
-                                currentEmojiIndex = Int(progress * CGFloat(emojis.count))
-                            }
-                        }
-                    }) {
-                        ZStack{
-                            Circle()
-                                .fill(Color.grey3)
-                                .frame(width: 50, height: 50)
-                                .overlay(Circle().stroke(Color.grey3, lineWidth: 0))
-                            
-                            Image(systemName: "plus")
-                                .font(.system(size: 35))
-                                .foregroundColor(.lightBlue)
-                        }
-                        //.padding(.leading)
+                        Image(systemName: "minus")
+                            .font(.system(size: 35))
+                            .foregroundColor(.lightBlue)
                     }
-                    
                 }
-       }
+                
+                Button(action: {
+                    withAnimation {
+                        updateProgress(increment: true)
+                    }
+                }) {
+                    ZStack{
+                        Circle()
+                            .fill(Color.grey3)
+                            .frame(width: 50, height: 50)
+                            .overlay(Circle().stroke(Color.grey3, lineWidth: 0))
+                        
+                        Image(systemName: "plus")
+                            .font(.system(size: 35))
+                            .foregroundColor(.lightBlue)
+                    }
+                }
             }
             .padding()
         }
     }
 }
-    struct ProgressTrack_Previews: PreviewProvider {
-        static var previews: some View {
-            ProgressTrack()
-            
+
+struct ProgressTrack_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressTrack()
+    }
 }
-        }
-    
-
-
-
-
-
-
